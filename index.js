1cdc2dbc98a8af65880b37b8229bdadc50e6b42d
Vinicius Gomes <viniplay0599@gmail.com>
2022-06-29 14:42:11 -0300

    Iniciando projeto

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+const bodyParser = require('body-parser');
 const app = require('./app');
 require('dotenv').config();
 
+const productRouter = require('./routes/productRouter');
+const salesRouter = require('./routes/salesRouter');
+
+app.use(bodyParser.json());
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
+
+app.use('/products', productRouter);
+
+app.use('/sales', salesRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
